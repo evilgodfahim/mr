c9d30ae6d147a5c4161bece3683bf325e5828805
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2026-04-01 00:59:35 +0600

    Update main1.py

--- main1.py
+++ main1.py
@@ -59,7 +59,7 @@ KL_API_FEEDS = set()
 
 # -- CONFIG --------------------------------------------------------------------
 
-DEDUP_MODEL           = "gemini-2.5-flash"
+DEDUP_MODEL           = "gemini-3-flash-preview"
 MISTRAL_MODEL         = "mistral-large-latest"
 PROCESSED_FILE        = "processed_articles_main.json"
 SELECTED_FILE         = "selected_articles_main.json"
